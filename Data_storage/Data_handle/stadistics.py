7708f2eff83215ffc1c4fceda2439333f5e0b98d
albertusku <ruiz17alberto@hotmail.com>
2024-12-16 14:55:07 +0100

    Adjust of the naming date of stadistics_day.xlxs

--- Data_storage/Data_handle/stadistics.py
+++ Data_storage/Data_handle/stadistics.py
@@ -4,13 +4,6 @@ from datetime import datetime
 
 def make_stadistics(df):
     #TODO list
-        #Resumen del dia anterior (solo se genera cuando empieza un nuevo dia) 
-            #Se tiene que guardar en un excel llamado Stadistics_day.xlxs, cada fila sera un dia
-            #Temperatura maxima y minima
-            #Temperatura media
-            #Precipitacion
-            #Maxima cantidad de precipitacion en 1h
-            #Velocidad maxima del viento
         #Resumen de lo que va de mes(solo se genera cuando empieza un nuevo dia)
             #Se tiene que guardar en un excel llamado Stadistics_month.xlxs, cada fila sera un mes
             #Temperatura maxima y minima indicando que dias fueron
@@ -21,9 +14,8 @@ def make_stadistics(df):
     last_day_data=df.iloc[-2]
     previous_day=last_day_data['Dia']
     previous_month=last_day_data['Mes']
-    make_stadistics_day(df,previous_day,previous_month) 
-    make_stadistics_month(df)
-    pass
+    df_data_day=make_stadistics_day(df,previous_day,previous_month) 
+    make_stadistics_month(df_data_day)
 
 def make_stadistics_day(df,day,month):
     excel_stadistics_day_path="/home/ruiz17/meteo/Data_storage/Data_handle/Stadistics_day.xlsx"
@@ -39,11 +31,19 @@ def make_stadistics_day(df,day,month):
     wind_speed_max=df[(df['Mes'] == month) & (df['Dia'] == day)]['wind_speed(m/s)'].max()
     values=[temp_max,tem_min,temp_mean,precipitaciones_totales,precipitacion_max_1h,wind_speed_max]
     data_day=dict(zip(keys,values))
-    new_data={"fecha":f"{day}-{month}"}
+    new_data={"Mes":month,"Dia":day}
     data_day={**new_data,**data_day}
     df_data_day=pd.DataFrame([data_day])
     from Data_storage.storage_builder import save_excel
-    save_excel(df_data_day,str(year),excel_stadistics_day_path)
+    return save_excel(df_data_day,str(year),excel_stadistics_day_path)
 
-def make_stadistics_month(df):
+def make_stadistics_month(df,month):
+    excel_stadistics_month_path="/home/ruiz17/meteo/Data_storage/Data_handle/Stadistics_month.xlsx"
+    now=datetime.now()
+    year=now.year
+    keys=["temp_max","tem_min","temp_mean","precipitaciones_totales","wind_speed_max"]
+    temp_max=df[(df['Mes']==month)]['temp (C)'].max()
+    #Hay que coger el mes actual del df y despues ir sacando toda la data que se necesita
     pass
+
+
