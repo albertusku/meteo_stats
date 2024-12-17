9f4ecece83bf3757fbede24e927216a5abae5b35
albertusku <ruiz17alberto@hotmail.com>
2024-12-17 19:52:55 +0100

    Implementacion de reintentos de peticion API

--- API/API_connection.py
+++ API/API_connection.py
@@ -24,42 +24,53 @@ def get_current_data(params):
     return data
 
 def get_data_from_api(name,url,params):
-    try:
-        if name=="weather_api":
-            response = requests.get(url, params=params)
-            response.raise_for_status()  # Lanza una excepción si hay un error HTTP
-            # Parsear la respuesta JSON
-            data = response.json()
-            with open(json_weather_api_output_path, "w") as file:
-                    json.dump(data, file, indent=4)  # Formato legible para humanos
-            # Extraer datos
-            humidity= data["main"]["humidity"]
-            pressure=data["main"]["pressure"]
-            wind_speed=data["wind"]["speed"]
-            # sunrise=data["sys"]["sunrise"]
-            #sunset=data["sys"]["sunset"]
-            return {"hum(%)":humidity,"pres(hPa)":pressure,"wind_speed(m/s)":wind_speed}
-        
-        elif name=="tomorrow_api":
-            url = f"https://api.tomorrow.io/v4/timelines?location={params['latitude']},{params['longitude']}&fields=precipitationIntensity,temperature&timesteps=30m&units=metric&apikey={params['api_key']}"
-            response = requests.get(url)
-            response.raise_for_status()  # Lanza una excepción si hay un error HTTP
-            # Parsear la respuesta JSON
-            data = response.json()
-            with open(json_tomorrow_api_output_path, "w") as file:
-                json.dump(data, file, indent=4)
-            intervals = data["data"]["timelines"][0]["intervals"]
-            # Extraer el primer intervalo (ejemplo de datos actuales)
-            first_interval = intervals[0]
-            precipitation = first_interval["values"].get("precipitationIntensity", "N/A")
-            temperature = first_interval["values"].get("temperature", "N/A")
+    attempt=0
+    max_retries=2
+    while attempt<max_retries:
+        try:
+            if name=="weather_api":
+                response = requests.get(url, params=params)
+                response.raise_for_status()  # Lanza una excepción si hay un error HTTP
+                # Parsear la respuesta JSON
+                data = response.json()
+                with open(json_weather_api_output_path, "w") as file:
+                        json.dump(data, file, indent=4)  # Formato legible para humanos
+                # Extraer datos
+                humidity= data["main"]["humidity"]
+                pressure=data["main"]["pressure"]
+                wind_speed=data["wind"]["speed"]
+                # sunrise=data["sys"]["sunrise"]
+                #sunset=data["sys"]["sunset"]
+                return {"hum(%)":humidity,"pres(hPa)":pressure,"wind_speed(m/s)":wind_speed}
+            
+            elif name=="tomorrow_api":
+                url = f"https://api.tomorrow.io/v4/timelines?location={params['latitude']},{params['longitude']}&fields=precipitationIntensity,temperature&timesteps=30m&units=metric&apikey={params['api_key']}"
+                response = requests.get(url)
+                response.raise_for_status()  # Lanza una excepción si hay un error HTTP
+                # Parsear la respuesta JSON
+                data = response.json()
+                with open(json_tomorrow_api_output_path, "w") as file:
+                    json.dump(data, file, indent=4)
+                intervals = data["data"]["timelines"][0]["intervals"]
+                # Extraer el primer intervalo (ejemplo de datos actuales)
+                first_interval = intervals[0]
+                precipitation = first_interval["values"].get("precipitationIntensity", "N/A")
+                temperature = first_interval["values"].get("temperature", "N/A")
 
-            # Devolver solo los valores de precipitationIntensity
-            return {"temp (C)": temperature,
-            "precipitation (mm/h)": precipitation}
+                # Devolver solo los valores de precipitationIntensity
+                return {"temp (C)": temperature,
+                "precipitation (mm/h)": precipitation}
 
-    except requests.exceptions.HTTPError as http_err:
-        return f"HTTP error occurred: {http_err}"
-    except requests.exceptions.RequestException as req_err:
-        return f"Request error occurred: {req_err}"
+        except requests.exceptions.HTTPError as http_err:
+            attempt+=1
+            if attempt<max_retries:
+                print(f"ERROR--->intento={attempt}")
+            else:
+                return f"HTTP error occurred: {http_err}"
+        except requests.exceptions.RequestException as req_err:
+            attempt+=1
+            if attempt<max_retries:
+                print(f"ERROR--->intento={attempt}")
+            else:
+                return f"Request error occurred: {req_err}"
         
