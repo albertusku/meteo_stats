762110b399f153b5a323b04bfb7291e245a95131
albertusku <ruiz17alberto@hotmail.com>
2024-12-19 11:54:00 +0100

    echo procces

--- push_to_raspi.sh
+++ push_to_raspi.sh
@@ -1,7 +1,10 @@
+echo "---------CREANDO BACKUP DE LA RASPI--------"
 scp -r ruiz17@pi:/home/ruiz17/meteo /home/ruiz17/meteo/BK #backup de todo el codigo
+echo "---------GUARDANDO DATOS IMPORTANTES--------"
 ./pull_data_to_PC.sh # Guardar datos antes de subir todo el codigo
 rm *.txt #Para borrar todos los txt donde estoy
 fecha=$(date)
 touch "${fecha}.txt" #Para poder idenfiticar el momento del push
+echo "---------SUBIENDO DATOS--------"
 scp -r /home/ruiz17/meteo ruiz17@pi:/home/ruiz17/
 
